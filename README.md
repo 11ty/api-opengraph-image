480dfc8947d8755073528cfe92b87fa4819c0bd0
Zach Leatherman <zachleatherman@gmail.com>
2021-10-27 16:57:38 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Open Graph Image API
+
+A runtime service to return optimized Open Graph images from a URL. Works with:
+
+1. `<meta name="og:image:secure_url">`
+1. `<meta name="og:image">`
+1. `<meta property="og:image">`
+1. `<meta name="twitter:image">`
+
+## Usage
+
+URLs have the formats:
+
+```
+/:url/
+```
+
+* `url` must be URI encoded.
+
+## Deploy your own
+
+<a href="https://app.netlify.com/start/deploy?repository=https://github.com/11ty/api-opengraph-image"><img src="https://www.netlify.com/img/deploy/button.svg" alt="Deploy to Netlify"></a>
+
---
+++ functions/og/ogImageHtml.js
@@ -0,0 +1,90 @@
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const EleventyImage = require("@11ty/eleventy-img");
+const EleventyCache = require("@11ty/eleventy-cache-assets");
+
+class OgImageHtml {
+  constructor(url) {
+    this.url = url;
+
+    if(!this.isFullUrl(url)) {
+      throw new Error(`Invalid \`url\`: ${url}`);
+    }
+  }
+
+  isFullUrl(url) {
+    try {
+      new URL(url);
+      return true;
+    } catch(e) {
+      // invalid url OR local path
+      return false;
+    }
+  }
+
+  async fetch() {
+    let response = await fetch(this.url);
+    let body = await response.text();
+    this.body = body;
+
+    this.$ = cheerio.load(body);
+    return body;
+  }
+
+  normalizePath(path) {
+    let u = new URL(path, this.url);
+    return u.href;
+  }
+
+  findImageUrls() {
+    let results = [];
+    let ogImageSecure = this.$("meta[name='og:image:secure_url']").attr("content");
+
+    if(ogImageSecure) {
+      results.push(ogImageSecure);
+    }
+
+    let ogImage = this.$("meta[name='og:image']").attr("content");
+    if(ogImage) {
+      results.push(ogImage);
+    }
+
+    // not sure if this is standardized or not
+    let ogImageProp = this.$("meta[property='og:image']").attr("content");
+    if(ogImageProp) {
+      results.push(ogImageProp);
+    }
+
+    let twitterImage = this.$("meta[name='twitter:image']").attr("content");
+    if(twitterImage) {
+      results.push(twitterImage);
+    }
+
+    return results;
+  }
+
+  async getImage(fallbackImageFormat) {
+    let width = 1200;
+    let images = this.findImageUrls();
+    if(images.length) {
+      return this.optimizeImage(images[0], width, fallbackImageFormat)
+    }
+  }
+
+  async optimizeImage(sharpInput, width, imageFormat) {
+    // normalize format
+    if(imageFormat && imageFormat === "svg+xml") {
+      imageFormat = "svg";
+    }
+
+    let stats = await EleventyImage(sharpInput, {
+      widths: [width],
+      formats: [imageFormat],
+      dryRun: true,
+    });
+
+    return stats;
+  }
+}
+
+module.exports = OgImageHtml;
