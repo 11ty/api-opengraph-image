c57c4735e71021700c19415eac39ea9cd1f2a787
Zach Leatherman <zachleat@users.noreply.github.com>
2024-08-28 09:25:59 -0500

    More YouTube exceptions

--- ogImageHtml.js
+++ ogImageHtml.js
@@ -28,10 +28,10 @@ class OgImageHtml {
       }
     });
     let body = await response.text();
-    console.log( body.split("<meta ").slice(1).map(line => `<meta ${line.slice(0, 200)}`) );
     this.body = body;
 
     this.$ = cheerio.load(body);
+
     return body;
   }
 
@@ -41,30 +41,40 @@ class OgImageHtml {
   }
 
   findImageUrls() {
-    let results = [];
-    let ogImageSecure = this.$("meta[name='og:image:secure_url']").attr("content");
+    let results = new Set();
 
-    if(ogImageSecure) {
-      results.push(ogImageSecure);
+    let cases = [
+      ["meta[name='og:image:secure_url']", "content"],
+      ["meta[name='og:image']", "content"],
+      ["meta[property='og:image']", "content"], // not sure if this is standardized
+      ["meta[name='twitter:image']", "content"],
+
+      // YouTube specific: https://github.com/11ty/api-opengraph-image/issues/6
+      ["link[rel='image_src']", "href"],
+      ["link[itemprop='thumbnailUrl']", "href"],
+      // ["link[itemprop='url']", "href"],
+    ];
+
+    for(let [selector, attribute] of cases) {
+      let imageUrl = this.$(selector).attr(attribute);
+      if(imageUrl) {
+        results.add(imageUrl);
+      }
     }
 
-    let ogImage = this.$("meta[name='og:image']").attr("content");
-    if(ogImage) {
-      results.push(ogImage);
+    // More YouTube specific stuff: https://github.com/11ty/api-opengraph-image/issues/6
+    let u = new URL(this.url);
+    if(results.size === 0 && u.host.endsWith(".youtube.com")) {
+      // Sizes borrowed from https://paulirish.github.io/lite-youtube-embed/testpage/poster-image-availability.html
+      // let sizes = ["maxresdefault", "sddefault", "hqdefault", "mqdefault", "default"];
+      let videoId = u.searchParams.get("v");
+      if(videoId) {
+        results.add(`https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`);
+      }
     }
 
-    // not sure if this is standardized or not
-    let ogImageProp = this.$("meta[property='og:image']").attr("content");
-    if(ogImageProp) {
-      results.push(ogImageProp);
-    }
-
-    let twitterImage = this.$("meta[name='twitter:image']").attr("content");
-    if(twitterImage) {
-      results.push(twitterImage);
-    }
-
-    return results;
+    console.log( "Found:", Array.from(results) );
+    return Array.from(results);
   }
 
   async getImages() {
