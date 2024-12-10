7bab35ab7320d25867211eff3c88c820c4577234
Zach Leatherman <zachleat@users.noreply.github.com>
2024-12-10 08:18:03 -0600

    Adds user-agent (improves results)

--- ogImageHtml.js
+++ ogImageHtml.js
@@ -21,7 +21,11 @@ class OgImageHtml {
   }
 
   async fetch() {
-    let response = await fetch(this.url);
+    let response = await fetch(this.url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15",
+      }
+    });
     let body = await response.text();
     this.body = body;
 
